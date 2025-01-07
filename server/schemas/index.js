68fa1cf9416f54a7df1537dee60b837a0f270134
venecoderr <147117046+venecoderr@users.noreply.github.com>
2025-01-06 19:17:54 -0500

    back end rework

--- server/schemas/index.js
+++
@@ -1,6 +0,0 @@
-// Import typeDefs and resolvers
-const typeDefs = require('./typeDefs');
-const resolvers = require('./resolvers');
-
-// Export them for use in your Apollo Server configuration
-module.exports = { typeDefs, resolvers };
